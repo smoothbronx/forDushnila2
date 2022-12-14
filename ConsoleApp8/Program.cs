a7456e43003ee8a8cd9e5c9ede08dd53591117da
vladik <215-u-9@DESKTOP-1RH5HHB>
2022-12-14 10:59:59 +0200

    Fixed Program.cs

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,9 +1,17 @@
 ﻿namespace ConsoleApp8 {
     class Program {
         public static void Main(string[] args) {
+
+            var rectangle = new Rectangle(12, 12);
             var roundedRectangle = new RoundedRectangle(6, 12, 12);
+
+            Console.WriteLine(rectangle.GetInformation());
             Console.WriteLine(roundedRectangle.GetInformation());
+
+            rectangle.MultByTwo();
             roundedRectangle.MultByTwo();
+
+            Console.WriteLine(rectangle.GetInformation());
             Console.WriteLine(roundedRectangle.GetInformation());
         }
     }
